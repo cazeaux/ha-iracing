c1e8d4df91b3da7c4f455cbca296157cf5a10621
Stephane Cazeaux <stephane.cazeaux@casper-online.net>
2024-03-10 10:37:31 +0000

    feat: support for 24S2 update with road licence split

--- custom_components/iracing/coordinator.py
+++ custom_components/iracing/coordinator.py
@@ -1,4 +1,5 @@
 """Data update coordinator for the Pronote integration."""
+
 from __future__ import annotations
 
 from datetime import date, timedelta
@@ -49,12 +50,18 @@ class IracingDataUpdateCoordinator(DataUpdateCoordinator):
         data = self.config_entry.data
         res = {
             "cust_id": data["cust_id"],
-            "road_licence_ir": None,
-            "road_licence_sr": None,
-            "road_starts": None,
-            "road_laps": None,
-            "road_wins": None,
-            "road_top5": None,
+            "sports_car_licence_ir": None,
+            "sports_car_licence_sr": None,
+            "sports_car_starts": None,
+            "sports_car_laps": None,
+            "sports_car_wins": None,
+            "sports_car_top5": None,
+            "formula_car_licence_ir": None,
+            "formula_car_licence_sr": None,
+            "formula_car_starts": None,
+            "formula_car_laps": None,
+            "formula_car_wins": None,
+            "formula_car_top5": None,
             "dirt_road_licence_ir": None,
             "dirt_road_licence_sr": None,
             "dirt_road_starts": None,
@@ -88,10 +95,15 @@ class IracingDataUpdateCoordinator(DataUpdateCoordinator):
             member_info = await self.hass.async_add_executor_job(
                 client.get_member, data["cust_id"]
             )
-            licence_road = next(
+            licence_sports_car = next(
                 x
                 for x in member_info["members"][0]["licenses"]
-                if x["category_id"] == 2
+                if x["category_id"] == 5
+            )
+            licence_formula_car = next(
+                x
+                for x in member_info["members"][0]["licenses"]
+                if x["category_id"] == 6
             )
             licence_dirt_road = next(
                 x
@@ -112,8 +124,11 @@ class IracingDataUpdateCoordinator(DataUpdateCoordinator):
             member_career = await self.hass.async_add_executor_job(
                 client.get_member_career, data["cust_id"]
             )
-            carrer_road = next(
-                x for x in member_career["stats"] if x["category_id"] == 2
+            carrer_sports_car = next(
+                x for x in member_career["stats"] if x["category_id"] == 5
+            )
+            carrer_formula_car = next(
+                x for x in member_career["stats"] if x["category_id"] == 6
             )
             carrer_dirt_road = next(
                 x for x in member_career["stats"] if x["category_id"] == 4
@@ -129,12 +144,19 @@ class IracingDataUpdateCoordinator(DataUpdateCoordinator):
                 client.get_recent_results, data["cust_id"]
             )
 
-            res["road_licence_ir"] = licence_road.get("irating", None)
-            res["road_licence_sr"] = licence_road["safety_rating"]
-            res["road_starts"] = carrer_road["starts"]
-            res["road_laps"] = carrer_road["laps"]
-            res["road_wins"] = carrer_road["wins"]
-            res["road_top5"] = carrer_road["top5"]
+            res["sports_car_licence_ir"] = licence_sports_car.get("irating", None)
+            res["sports_car_licence_sr"] = licence_sports_car["safety_rating"]
+            res["sports_car_starts"] = carrer_sports_car["starts"]
+            res["sports_car_laps"] = carrer_sports_car["laps"]
+            res["sports_car_wins"] = carrer_sports_car["wins"]
+            res["sports_car_top5"] = carrer_sports_car["top5"]
+
+            res["formula_car_licence_ir"] = licence_formula_car.get("irating", None)
+            res["formula_car_licence_sr"] = licence_formula_car["safety_rating"]
+            res["formula_car_starts"] = carrer_formula_car["starts"]
+            res["formula_car_laps"] = carrer_formula_car["laps"]
+            res["formula_car_wins"] = carrer_formula_car["wins"]
+            res["formula_car_top5"] = carrer_formula_car["top5"]
 
             res["dirt_road_licence_ir"] = licence_dirt_road.get("irating", None)
             res["dirt_road_licence_sr"] = licence_dirt_road["safety_rating"]
